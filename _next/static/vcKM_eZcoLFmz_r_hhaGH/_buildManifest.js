3766287d16c5839d959fa227325aac0f7d567e58
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-24 04:48:33 +0000

    Deploy hgoldgrabe/portfolio to github.com/hgoldgrabe/portfolio.git:gh-pages

---
+++ _next/static/vcKM_eZcoLFmz_r_hhaGH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-b98e1e1eaa240a803f62.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-098dadf68712d2668b84.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
