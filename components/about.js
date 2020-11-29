d88f38af6a6f7a22e6e9ca9fbc73b79962de91a1
nicholasvrdoljak <admin@nicholasvrdoljak.com>
2020-11-28 17:25:32 -0700

    Font

--- components/about.js
+++ components/about.js
@@ -3,8 +3,9 @@ import Fade from 'react-reveal/Fade';
 const styles = {
     section: {
         height: '100vh',
-        backgroundColor: 'purple',
-        fontFamily: "'Open Sans', sans-serif",
+        backgroundColor: '#DAF7F8',
+        display: 'flex',
+        flexDirection: 'column'
     }
 }
 
@@ -12,7 +13,8 @@ function About() {
 	return (
 		<div id='about' style={styles.section}>
             <Fade bottom>
-                <div>Hi, I’m Holly. I’m a communications specialist with a background in higher education and small business marketing. I love producing clear messaging paired with simple, clean design. My talents include print and digital layout, design, copywriting, and editing. If you’re in the market to streamline your marketing materials and build a community around your brand, I would love to get in touch.</div>
+                <div style={{fontFamily: "Cherolina", fontSize: '4em', alignSelf: 'center'}}>Hi, I’m Holly</div>
+                <div style={{fontFamily: "'Open Sans', sans-serif"}}>I’m a communications specialist with a background in higher education and small business marketing. I love producing clear messaging paired with simple, clean design. My talents include print and digital layout, design, copywriting, and editing. If you’re in the market to streamline your marketing materials and build a community around your brand, I would love to get in touch.</div>
             </Fade>
 		</div>
 	);
--- pages/_document.js
+++ pages/_document.js
@@ -2,19 +2,25 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
 
-  render () {
-    return (
-      <Html style={{scrollBehavior: 'smooth'}}>
-        <Head>
-          <meta charSet="UTF-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans|Work+Sans:300" rel="stylesheet"/>
-        </Head>
-        <body style={{margin:0}}>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render () {
+		return (
+			<Html style={{scrollBehavior: 'smooth'}}>
+			<Head>
+				<meta charSet="UTF-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+				<link href="https://fonts.googleapis.com/css?family=Open+Sans|Work+Sans:300" rel="stylesheet"/>
+				<style jsx>{`
+					@font-face {
+						font-family: "Cherolina";
+						src: url("/Cherolina.ttf");
+					}
+				`}</style>
+			</Head>
+			<body style={{margin:0}}>
+				<Main />
+				<NextScript />
+			</body>
+			</Html>
+		)
+	}
 }
