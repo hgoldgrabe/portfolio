fb04feeade3cd3bc4edb6d32d18153864b6393c2
nicholasvrdoljak <vrdoljaknicholas@gmail.com>
2020-12-01 02:59:24 +0000

    Deploying to gh-pages from  @ 94e71804413419f3a830c8a1a585af74da7998e4 🚀

---
+++ _next/static/vpSkbeHjorNSFpfhSrroG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-cee7d9c1503358061d3c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-857ef95028c9902cc64c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
